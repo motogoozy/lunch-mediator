8a2294c30b869549b170c14a924923e683202f64
Kyle Payne <kspayne93@gmail.com>
2020-07-08 01:01:47 -0600

    add Switch, Routes, add global view styhle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.scss';
 import './globals.scss';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from './views/LandingPage/LandingPage';
 
 function App() {
    return (
       <div className="App">
-
+         <Switch>
+            <Route component={LandingPage} exact path='/' />
+         </Switch>
       </div>
    );
 }
